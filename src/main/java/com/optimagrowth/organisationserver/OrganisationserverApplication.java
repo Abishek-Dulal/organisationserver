143d16b10c624e654f4c6163e7a7f277fabf8043
Abishek Dulal <abishek.dulal@sbsolutionsnepal.com>
2022-04-02 13:28:20 +0545

    Initial commit

---
+++ src/main/java/com/optimagrowth/organisationserver/OrganisationserverApplication.java
@@ -0,0 +1,13 @@
+package com.optimagrowth.organisationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganisationserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrganisationserverApplication.class, args);
+    }
+
+}
